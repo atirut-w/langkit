778a5b737f5fdc353d86a9e98d9127fe47c2e6c3
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-05-24 23:57:47 +0700

    Implement lexer constructor

--- include/langkit/lexer.hpp
+++ include/langkit/lexer.hpp
@@ -1,9 +1,19 @@
 #pragma once
 #include <langkit/ipositional.hpp>
+#include <memory>
+#include <istream>
+#include <filesystem>
 
 namespace LangKit
 {
     class Lexer : protected IPositional
     {
+    private:
+        std::unique_ptr<std::istream> stream;
+    
+    public:
+        Lexer() = delete;
+        Lexer(const std::filesystem::path& path);
+        Lexer(std::unique_ptr<std::istream> stream);
     };
 }
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,1 +1,24 @@
 #include <langkit/lexer.hpp>
+#include <stdexcept>
+#include <fstream>
+
+using namespace std;
+using namespace LangKit;
+
+Lexer::Lexer(const filesystem::path& path)
+{
+    if (!filesystem::exists(path))
+    {
+        throw invalid_argument("path does not exist");
+    }
+    stream = move(make_unique<ifstream>(path));
+}
+
+Lexer::Lexer(std::unique_ptr<std::istream> stream)
+{
+    if (!*stream)
+    {
+        throw invalid_argument("stream is not valid");
+    }
+    this->stream = move(stream);
+}
